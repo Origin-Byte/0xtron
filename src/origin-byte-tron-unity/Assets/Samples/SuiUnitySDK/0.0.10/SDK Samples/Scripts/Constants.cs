378b73fa2aa9e6a98279481fa25f4715e25b2022
Tamas Szucs <tamas@originbyte.io>
2022-10-20 01:02:45 +0200

    reference published move contracts

--- src/origin-byte-tron-unity/Assets/Samples/SuiUnitySDK/0.0.10/SDK Samples/Scripts/Constants.cs
+++ src/origin-byte-tron-unity/Assets/Samples/SuiUnitySDK/0.0.10/SDK Samples/Scripts/Constants.cs
@@ -1,7 +1,7 @@
 public class Constants
 {    
-    public const string PACKAGE_OBJECT_ID = "0xa28c569cb1e272ad0d0c18b5ad2f48fa0c4e425e";
+    public const string PACKAGE_OBJECT_ID = "0x7dd62039a3b9fb56766fae089ca82dfa1dc104bc";
     public const string ONCHAIN_STATE_OBJECT_ID_KEY = PACKAGE_OBJECT_ID + "_onChainStateObject";
     public const string MODULE_NAME = "playerstate_module";
-    public const string SCOREBOARD_OBJECT_ID = "0x1611d872782c5514a37024117cb93fdc1e35b329";
+    public const string SCOREBOARD_OBJECT_ID = "0x5f16a2153880e4445918181d43506969eb38c964";
 }
