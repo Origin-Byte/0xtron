ea3e26da4b90954f09c390f9c9f3496660a1f4d9
Tamas Szucs <tamas@originbyte.io>
2022-10-24 17:39:59 +0200

    update to sui 0.12.2 devnet deployment

--- src/origin-byte-tron-unity/Assets/Scripts/Constants.cs
+++ src/origin-byte-tron-unity/Assets/Scripts/Constants.cs
@@ -1,7 +1,7 @@
 public class Constants
 {    
-    public const string PACKAGE_OBJECT_ID = "0x7dd62039a3b9fb56766fae089ca82dfa1dc104bc";
+    public const string PACKAGE_OBJECT_ID = "0x213a1fe26bbfea3dd027c5c92cf0008a870dad70";
     public const string ONCHAIN_STATE_OBJECT_ID_KEY = PACKAGE_OBJECT_ID + "_onChainStateObject";
     public const string MODULE_NAME = "playerstate_module";
-    public const string SCOREBOARD_OBJECT_ID = "0x5f16a2153880e4445918181d43506969eb38c964";
+    public const string SCOREBOARD_OBJECT_ID = "0x619bdf758b5b546abbf050ff7b1451a8acbe435c";
 }
