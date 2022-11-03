31c4563e682e63fdaaa3dde6f8619c59e4a62060
Tamas Szucs <tamas@originbyte.io>
2022-11-03 15:27:40 +0100

    update to devnet 0.14

--- src/origin-byte-tron-unity/Assets/Scripts/Constants.cs
+++ src/origin-byte-tron-unity/Assets/Scripts/Constants.cs
@@ -1,8 +1,8 @@
 public class Constants
 {    
-    public const string PACKAGE_OBJECT_ID = "0x74c82f242445275fa0f57590f85ca7a63ecfba99";
+    public const string PACKAGE_OBJECT_ID = "0x51c27b3ad8223dbef3a21f4c71ff9466c7936c4";
     public const string ONCHAIN_STATE_OBJECT_ID_KEY = PACKAGE_OBJECT_ID + "_onChainStateObject";
     public const string MODULE_NAME = "playerstate_module";
-    public const string SCOREBOARD_OBJECT_ID = "0x59a0cac84c707cb2c89c350a180af4bc0ff2f99c";
+    public const string SCOREBOARD_OBJECT_ID = "0x6ba17c534b80ba414a606e76a3c0fd1c9f14f25c";
     public const string GAS_OBJECT_ID_KEY = "gasObjectId";
 }
