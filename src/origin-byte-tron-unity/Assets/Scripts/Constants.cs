a8367eac1c9b7c00fab6f6560d1b5d798c9e985e
Tamas Szucs <tomfurrier@gmail.com>
2022-11-11 16:22:48 +0100

    Update Constants.cs

--- src/origin-byte-tron-unity/Assets/Scripts/Constants.cs
+++ src/origin-byte-tron-unity/Assets/Scripts/Constants.cs
@@ -1,8 +1,8 @@
 public class Constants
 {    
-    public const string PACKAGE_OBJECT_ID = "0x0c306c546ada58c2b0ab422033a432e9371c6de4";
+    public const string PACKAGE_OBJECT_ID = "0x1d4214eb303cc27007022d738f8a2ab49597f1e7";
     public const string ONCHAIN_STATE_OBJECT_ID_KEY = PACKAGE_OBJECT_ID + "_onChainStateObject";
     public const string MODULE_NAME = "playerstate_module";
-    public const string SCOREBOARD_OBJECT_ID = "0xf5ece5ed67159861e85dd6fdb955ba9f95436592";
+    public const string SCOREBOARD_OBJECT_ID = "0xf6fffaba1c5a76f112752e5811fd25f4ae5723e5";
     public const string GAS_OBJECT_ID_KEY = "gasObjectId";
 }
