7474566b9a80b61f98b25e97c978e8e671633e7c
Tamas Szucs <tamas@originbyte.io>
2022-12-01 16:38:24 +0100

    Update OnChainStateStore.cs

--- src/origin-byte-tron-unity/Assets/Scripts/OnChainStateStore.cs
+++ src/origin-byte-tron-unity/Assets/Scripts/OnChainStateStore.cs
@@ -43,14 +43,24 @@ public class OnChainStateStore : MonoBehaviour
     
     private void SetLocalPlayerAddress()
     {
-        if (string.IsNullOrWhiteSpace(_localPlayerAddress))
+        if (IsInvalidAddress(_localPlayerAddress))
         {
             _localPlayerAddress = SuiWallet.GetActiveAddress();
         }
-        if (string.IsNullOrWhiteSpace(_localPlayerAddress))
+        if (IsInvalidAddress(_localPlayerAddress))
         {
             Debug.LogError("Could not retrieve active Sui address");
         }
+    }
+
+    private static bool IsInvalidAddress(string address)
+    {
+        return IsEmptyAddress(address) || address != SuiWallet.GetActiveAddress();
+    }
+    
+    private static bool IsEmptyAddress(string address)
+    {
+        return string.IsNullOrWhiteSpace(address) || address == "0x";
     }
     
     private void OnCloseWebSocketConnection()
@@ -61,9 +71,6 @@ public class OnChainStateStore : MonoBehaviour
 
     private void OnWebSocketEvent(string message)
     {
-//        Debug.Log(message);
-        SetLocalPlayerAddress();
-
         var messageData = JsonConvert.DeserializeObject<WebsocketMessage>(message);
         var eventData = messageData?.Params?.Result;
         if (eventData?.Event.MoveEvent == null) return;
@@ -91,7 +98,17 @@ public class OnChainStateStore : MonoBehaviour
         {
             if (isLocalSender && isExploded)
             {
-                States.Remove(sender);
+                // double check again, todo refact
+                SetLocalPlayerAddress();
+                isLocalSender = sender == _localPlayerAddress;
+                if (isLocalSender)
+                {
+                    States.Remove(sender);
+                }
+                else if (sequenceNumber > States[sender].SequenceNumber)
+                {
+                    States[sender] = state;
+                }
             }
             else if (sequenceNumber > States[sender].SequenceNumber)
             {
@@ -109,97 +126,6 @@ public class OnChainStateStore : MonoBehaviour
         // cube.transform.position = position.ToVector3() + Vector3.back;
     }
 
-    // todo move this to sdk
-    public class WebsocketMessage
-    {
-        public string Method { get; set; }
-
-        public WebsocketMessageParams Params { get; set; }
-    }
-
-    public class WebsocketMessageParams
-    {
-        public ulong Subscription { get; set; }
-        public SuiEventEnvelope Result { get; set; }
-    }
-    
-    private async Task GetOnChainUpdateEventsAsync()
-    {
-//        Debug.Log("GetOnChainUpdateEventsAsync");
-        SetLocalPlayerAddress();
-        var query = new SuiMoveEventEventQuery()
-        {
-            MoveEvent = $"{Constants.PACKAGE_OBJECT_ID}::playerstate_module::PlayerStateUpdatedEvent"
-        };
-        
-        RpcResult<SuiPage_for_EventEnvelope_and_EventID> rpcResult;
-
-        // if (_latestEvent != null)
-        // {
-        //     rpcResult = await SuiApi.Client.GetEventsAsync(query, _nextCursor, 20, false);
-        // }
-        // else
-        { 
-            // start from the latest event
-            rpcResult = await SuiApi.Client.GetEventsAsync(query, null, 20, true);
-        }
-        
-        Debug.Log(JsonConvert.SerializeObject(rpcResult));
-        if (rpcResult != null && rpcResult.IsSuccess)
-        {
-            foreach (var eventData in rpcResult.Result.Data)
-            {
-                if (eventData.Event.MoveEvent != null)
-                {
-                    //Debug.Log("GetOnChainUpdateEventsAsync: " + JsonConvert.SerializeObject(eventData.Event.MoveEvent));
-
-                    var sender = eventData.Event.MoveEvent.Sender;
-                    var bcs = eventData.Event.MoveEvent.Bcs;
-                    var timeStamp = eventData.Timestamp;
-
-                    // BCS conversion
-                    var bytes = Convert.FromBase64String(bcs);
-                    var posX64 = BitConverter.ToUInt64(bytes, 0);
-                    var posY64 = BitConverter.ToUInt64(bytes, 8);
-                    var velX64 = BitConverter.ToUInt64(bytes, 16);
-                    var velY64 = BitConverter.ToUInt64(bytes, 24);
-                    var sequenceNumber = BitConverter.ToUInt64(bytes, 32);
-                    var isExploded = BitConverter.ToBoolean(bytes, 40);
-
-                    var position = new OnChainVector2(posX64, posY64);
-                    var velocity = new OnChainVector2(velX64, velY64);
-
-                    var state = new OnChainPlayerState(position, velocity, sequenceNumber, isExploded);
-
-                    bool isLocalSender = sender == _localPlayerAddress;
-                    
-                    if (States.ContainsKey(sender)) 
-                    {
-                        if (isLocalSender && isExploded)
-                        {
-                            States.Remove(sender);
-                        }
-                        else if (sequenceNumber > States[sender].SequenceNumber)
-                        {
-                            States[sender] = state;
-                        }
-                    }
-                    else if (!isExploded)
-                    {
-                        States.Add(sender, state);
-                    }
-                    
-                   // Debug.Log($"OnChainUpdate: {position.ToVector3()}. sequenceNumber: {sequenceNumber}. sender: {sender}. isExploded:{ isExploded}. States.ContainsKey(sender): {States.ContainsKey(sender)} ");
-                    // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                    // cube.transform.position = position.ToVector3() + Vector3.back;
-
-                }
-            }
-        }
-
-        UpdateRemotePlayers();
-    }
-
     private void UpdateRemotePlayers()
     {
         foreach (var state in States)
@@ -208,15 +134,22 @@ public class OnChainStateStore : MonoBehaviour
             {
                 if (!_remotePlayers.ContainsKey(state.Key))
                 {
-                    var remotePlayerGo = Instantiate(remotePlayerPrefab, playersParent);
-                    remotePlayerGo.ownerAddress = state.Key;
-                    remotePlayerGo.GetComponent<ExplosionController>().explosionRoot = explosionsParent;
-                    remotePlayerGo.gameObject.SetActive(true);
-                    _remotePlayers.Add(state.Key, remotePlayerGo);
-                    
-                    var trailColliderGo =  Instantiate(trailColliderPrefab, trailCollidersParent);
-                    trailColliderGo.ownerAddress = state.Key;
-                    trailColliderGo.gameObject.SetActive(true);
+                    // not that nice logic, re-check to see if we changed addresses since last set (re-import etc).
+                    SetLocalPlayerAddress();
+                    if (state.Key != _localPlayerAddress)
+                    {
+                        Debug.Log(
+                            $"UpdateRemotePlayers - spawning new player: state.Key:{state.Key}, _localPlayerAddress: {_localPlayerAddress}");
+                        var remotePlayerGo = Instantiate(remotePlayerPrefab, playersParent);
+                        remotePlayerGo.ownerAddress = state.Key;
+                        remotePlayerGo.GetComponent<ExplosionController>().explosionRoot = explosionsParent;
+                        remotePlayerGo.gameObject.SetActive(true);
+                        _remotePlayers.Add(state.Key, remotePlayerGo);
+
+                        var trailColliderGo = Instantiate(trailColliderPrefab, trailCollidersParent);
+                        trailColliderGo.ownerAddress = state.Key;
+                        trailColliderGo.gameObject.SetActive(true);
+                    }
                 }
             }
         }
