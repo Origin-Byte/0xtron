716097518553bd8af5b9e1ee2845f13f49278fe8
Tamas Szucs <tamas@originbyte.io>
2022-12-05 17:04:29 +0100

    enable/disable minimap

--- src/origin-byte-tron-unity/Assets/Scripts/MinimapManager.cs
+++ src/origin-byte-tron-unity/Assets/Scripts/MinimapManager.cs
@@ -3,14 +3,21 @@ using UnityEngine;
 public class MinimapManager : MonoBehaviour
 {
     public GameObject minimap;
-
+    public Camera minimapCamera;
+    
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
             if (minimap != null)
             {
-                minimap.SetActive(!minimap.activeSelf);
+                var isMinimapActive = minimap.activeSelf;
+                minimap.SetActive(!isMinimapActive);
+
+                if (minimapCamera != null)
+                {
+                    minimapCamera.gameObject.SetActive(!isMinimapActive);
+                }
             }
         }
     }
