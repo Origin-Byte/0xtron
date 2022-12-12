c947e682156ddac0a443b9735bbb6fe0cd6b73e8
Tamas Szucs <tamas@originbyte.io>
2022-12-12 16:29:29 +0100

    improve play button style

--- src/origin-byte-tron-unity/Assets/Scripts/UI2/CustomButton.cs
+++ src/origin-byte-tron-unity/Assets/Scripts/UI2/CustomButton.cs
@@ -1,10 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
+[RequireComponent(typeof(Image))]
 public class CustomButton : Button
 {
     public AudioClip hoverSound;
@@ -16,6 +15,11 @@ public class CustomButton : Button
     public Color hoveredTextColor;
     public Color pressedTextColor;
 
+    public Sprite defaultImage;
+    public Sprite hoveredImage;
+
+    private Image _image;
+    
     protected override void Start()
     {
         base.Start();
@@ -23,18 +27,28 @@ public class CustomButton : Button
         {
             audioSource.PlayOneShot(clickSound);
         });
+        _image = GetComponent<Image>();
     }
-    
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        _image.sprite = defaultImage;
+        text.color = defaultTextColor;
+    }
+
     public override void OnPointerEnter(PointerEventData eventData)
     {
         base.OnPointerEnter(eventData);
         audioSource.PlayOneShot(hoverSound);
         text.color = hoveredTextColor;
+        _image.sprite = hoveredImage;
     }
 
     public override void OnPointerExit(PointerEventData eventData)
     {
         base.OnPointerExit(eventData);
         text.color = defaultTextColor;
+        _image.sprite = defaultImage;
     }
 }
